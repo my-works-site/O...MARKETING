16d7bdd98e0f5f7cdcc5e3ab4b0432e45c79220c
Paddington <max-basarin@ya.ru>
2021-07-25 14:47:01 +0300

    added block slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
   var mixer = mixitup(".portfolio__content");
+
+  $(".slider-blog__inner").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
+  });
 });
