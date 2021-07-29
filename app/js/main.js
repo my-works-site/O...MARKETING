a222e307f7561b63846069fce830b30655ba1caf
Paddington <max-basarin@ya.ru>
2021-07-29 17:38:08 +0300

    added adaptive slader

--- app/js/main.js
+++ app/js/main.js
@@ -17,5 +17,13 @@ $(function () {
       '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
